0b9767ebc1dde5d7d3f637b63fe7cff121736db6
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2025-12-01 15:35:23 +0530

    bing  added

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,7 +26,7 @@ export const config = {
          * - public folder
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|BingSiteAuth.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
 
